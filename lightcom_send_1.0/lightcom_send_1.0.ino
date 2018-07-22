391770ebe97d807b6106e36d8d73f43b3d7e46c0
入江嵐士郎 <ranshiroirie@gmail.com>
2018-07-23 01:32:44 +0900

    add arduino file

---
+++ lightcom_send_1.0/lightcom_send_1.0.ino
@@ -0,0 +1,100 @@
+#define DMS 30000
+#define BIT 300
+#define passlength 8
+
+const int ledPin = 12;  // Analog input pin that the potentiometer is attached to
+
+unsigned char pulse[BIT];
+int pass[passlength] = {1, 0, 1, 0, 1, 0, 0, 1};
+int endpass[8] = {1, 0, 1, 0, 1, 0, 1, 1};
+byte datasize;
+byte sendsize;
+byte digit[9];
+int binary[8] = {128, 64, 32, 16, 8, 4, 2, 1};
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(19200);
+  Serial.println("送りたい文字を入力して送信ボタンを押してください");
+}
+
+void loop() {
+  Serial.println("Start");
+  
+  for (int j = 0; j < passlength; j++) {
+    if (pass[j] == 1) {
+      digitalWrite(ledPin, HIGH);
+      delayMicroseconds(DMS);
+    } else {
+      digitalWrite(ledPin, LOW);
+      delayMicroseconds(DMS);
+    }
+    Serial.print(pass[j]);
+  }
+  Serial.println();
+
+  spels(); //入力された文字の変換
+  
+  sending(); //文字の送信
+  
+  for (int k = 0; k < 8; k++) {
+    if (endpass[k] == 1) {
+      digitalWrite(ledPin, HIGH);
+      delayMicroseconds(DMS);
+    } else {
+      digitalWrite(ledPin, LOW);
+      delayMicroseconds(DMS);
+    }
+    Serial.print(endpass[k]);
+  }
+  Serial.println();
+  
+  Serial.println("Finish");
+}
+
+//入力された文字の変換------------------------------------------------------------
+void spels() {
+  datasize = Serial.available();
+  if (datasize > 0) {
+    delay(20);
+    datasize = Serial.available();
+    sendsize = datasize;
+
+    Serial.print("入力された文字数:");
+    Serial.println(datasize);
+
+    for (byte i = 0; i < sendsize; i++) {
+      digit[0] = Serial.read();
+      Serial.print("文字のASCIIコード:");
+      Serial.println(digit[0]);
+      for (int j = 0; j < 8; j++) {
+        if (digit[j] >= binary[j]) {
+          pulse[i * 8 + j] = 1;
+          digit[j + 1] = digit[j] - binary[j];
+        } else {
+          pulse[i * 8 + j] = 0;
+          digit[j + 1] = digit[j];
+        }
+        Serial.print(pulse[i * 8 + j]);
+      }
+      Serial.println();
+    }
+  }
+}
+
+//変換した文字の送信------------------------------------------------------------
+void sending() {
+  for (byte i = 0; i < sendsize; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (digit[j] == 1) {
+        digitalWrite(ledPin, HIGH);
+        delayMicroseconds(DMS);
+      } else {
+        digitalWrite(ledPin, LOW);
+        delayMicroseconds(DMS);
+      }
+      Serial.print(pulse[i * 8 + j]);
+    }
+    Serial.println();
+  }
+}
