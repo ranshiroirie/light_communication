baf758ad5ea1e2a28da20d33469a51629ede4ac4
入江嵐士郎 <ranshiroirie@gmail.com>
2018-07-23 02:24:23 +0900

    add comments

--- lightcom_send_1.0/lightcom_send_1.0.ino
+++ lightcom_send_1.0/lightcom_send_1.0.ino
@@ -1,12 +1,12 @@
 #define DMS 30000
 #define BIT 300
-#define passlength 8
+#define passlength 8 //パスの長さ　変更が可能だが送信側と合わせる必要あり　デフォルトは8
 
-const int ledPin = 12;  // Analog input pin that the potentiometer is attached to
+const int ledPin = 12;  //LEDのピン番号
 
 unsigned char pulse[BIT];
-int pass[passlength] = {1, 0, 1, 0, 1, 0, 0, 1};
-int endpass[8] = {1, 0, 1, 0, 1, 0, 1, 1};
+int pass[passlength] = {1, 0, 1, 0, 1, 0, 0, 1}; //パスの配列　配列の個数はpasslengthで指定した数と、中身は受信側のものと合わせる　
+int endpass[8] = {1, 0, 1, 0, 1, 0, 1, 1}; //読み込みの終了を合図するASCIIコードの二進数にした配列
 byte datasize;
 byte sendsize;
 byte digit[9];
